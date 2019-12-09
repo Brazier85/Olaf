2c75a5ee84abe5edf18646c5e0f81e3498e3c7d9
Ferdinand Berger <ferdinand.berger@sick.de>
2019-12-09 09:19:38 +0100

    cleanup populate

--- Olaf/populate.js
+++ Olaf/populate.js
@@ -1,3 +1,30 @@
+let harvester = {
+    memory: {memory: {role: 'harvester'}},
+    body: [WORK,CARRY,MOVE,MOVE],
+    name: "Harvester_" + Game.time
+}
+
+let builder = {
+    memory: {memory: {role: 'builder'}},
+    body: [WORK,CARRY,MOVE],
+    name: "Builder_" + Game.time
+}
+
+let upgrader = {
+    memory: {memory: {role: 'upgrader'}},
+    body: [WORK,CARRY,MOVE],
+    name: "Upgrader_" + Game.time
+}
+
+function doSpawn(creeptype) {
+    if(Game.spawns['Spawn1'].spawning) {
+        //Is spawning have to wait
+    } else {
+        Game.spawns['Spawn1'].spawnCreep(creeptype.body, creeptype.name, creeptype.memory);
+    }
+}
+
+
 function PopulateRoom() {
 
     // Check if there are two harvesters
@@ -6,20 +33,14 @@ function PopulateRoom() {
     var builders = _(Game.creeps).filter( { memory: { role: 'builder' } } ).size();
 
     if ( harvesters < 2 ) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
-            {memory: {role: 'harvester'}});
+        console.log('Spawning new harvester');
+        doSpawn(harvester);
     } else if ( upgraders < 1 ) {
-        var newName = 'Upgrader' + Game.time;
-        console.log('Spawning new upgrader: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
-            {memory: {role: 'upgrader'}});
+        console.log('Spawning new upgrader');
+        doSpawn(upgrader);
     } else if ( builders < 1 ) {
-        var newName = 'Builder' + Game.time;
-        console.log('Spawning new builder: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
-            {memory: {role: 'builder'}});
+        console.log('Spawning new builder');
+        doSpawn(builder);
     } 
 }
 
