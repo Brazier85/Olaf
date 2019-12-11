19db23c30ad6c1c1da4a1cec37d30c3e6d05e756
Ferdinand Berger <ferdy2003@gmail.com>
2019-12-11 09:33:55 +0100

    Miner fix wenn kein carrier da ist

--- Olaf/CreepMiner.js
+++ Olaf/CreepMiner.js
@@ -37,6 +37,32 @@ CreepMiner.prototype.act = function() {
 	if(this.creep.store[RESOURCE_ENERGY] == this.creep.store.getCapacity()) {
 		// Hier muss eine Prüfung rein ob ein Carrier in der Nähe ist.
 		// Muss der Miner zum Spawn laufen und ausladen
+		var creepsNear = this.creep.pos.findInRange(FIND_MY_CREEPS, 5);
+		var carrierinRange = false;
+		if(creepsNear.length){
+			for(var n in creepsNear){
+				//Wenn Miner dann Energie abholen
+				if(creepsNear[n].memory.role === 'CreepCarrier'){
+					carrierinRange = true;
+				}
+			}
+		}
+
+		if(carrierinRange) {
+			//Alles okay
+		} else {
+			var targets = this.creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0) {
+                if(this.creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    this.creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+		}
 	} else {
 		if(this.creep.harvest(this.resource) == ERR_NOT_IN_RANGE) {
 			this.creep.moveTo(this.resource);
