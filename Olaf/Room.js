29a3f8621dbc22e4d9b187e226a465ae466d8fc9
Ferdinand Berger <bergefe@deagomac0007.fritz.box>
2019-12-13 22:42:35 +0100

    nur bis 50% energie

--- Olaf/Room.js
+++ Olaf/Room.js
@@ -188,8 +188,8 @@ Room.prototype.defendRoom = function() {
 	// Wenn keine Gegner da sind
 	if(hostiles.length === 0) {
 		towers.forEach(tower => {
-			// Nur bis 10% Energie weg sind
-			if(tower.store[RESOURCE_ENERGY] > (tower.store.getFreeCapacity(RESOURCE_ENERGY)/10)*9){
+			// Nur bis 50% Energie
+			if(tower.store[RESOURCE_ENERGY] > tower.store.getFreeCapacity(RESOURCE_ENERGY){
 				var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL && s.structureType != STRUCTURE_RAMPART});
 				if(closestDamagedStructure) {
 					 tower.repair(closestDamagedStructure);
