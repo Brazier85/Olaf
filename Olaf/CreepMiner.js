f0521df612ff77d260cc7ecf09656eec42aebf2c
Ferdinand Berger <bergefe@deagomac0007.fritz.box>
2019-12-14 20:27:26 +0100

    Miner allways stand on container

--- Olaf/CreepMiner.js
+++ Olaf/CreepMiner.js
@@ -88,15 +88,7 @@ CreepMiner.prototype.act = function() {
 }
 
 CreepMiner.prototype.checkContainer = function() {
-	var containerNear = this.creep.pos.findInRange(FIND_STRUCTURES, 0, {
-		filter: function(s) {
-				if ((s.structureType == STRUCTURE_CONTAINER) && (s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-		});
+	var containerNear = this.creep.pos.findInRange(FIND_STRUCTURES,{filter: (s) => s.structureType == STRUCTURE_CONTAINER})
 	if (containerNear.length) {
 		this.remember('harvest', HARVEST.DROP);
 	} else {
