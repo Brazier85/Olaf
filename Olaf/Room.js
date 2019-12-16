3b0bca5e064dd3df991300a081474a49c91b96d1
Ferdinand Berger <bergefe@deagomac0007.fritz.box>
2019-12-16 18:47:07 +0100

    changed range back to 20

--- Olaf/Room.js
+++ Olaf/Room.js
@@ -189,7 +189,7 @@ Room.prototype.defendRoom = function() {
 		var setFlag = true;
         //Game.notify(`User ${username} spotted in room ${this.room.name}`);
         towers.forEach(tower => {
-			var EnemysInRange = tower.pos.findInRange(FIND_HOSTILE_CREEPS,15);
+			var EnemysInRange = tower.pos.findInRange(FIND_HOSTILE_CREEPS,20);
 			if (EnemysInRange.length) {
 				tower.attack(EnemysInRange[0]);
 				setFlag = false;
