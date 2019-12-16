68267c19f9628147383b193dc14e75d0f97930c3
Ferdinand Berger <ferdinand.berger@sick.de>
2019-12-16 10:12:46 +0100

    own attributes for miners

--- Olaf/CreepFactory.js
+++ Olaf/CreepFactory.js
@@ -74,6 +74,37 @@ CreepFactory.prototype.new = function(creepType, spawn) {
 
 	switch(creepType) {
 		case 'CreepMiner':
+			if(level <= 1) {
+				abilities = [WORK, CARRY, MOVE];
+			} else
+			if(level <= 2) {
+				abilities = [WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 3) {
+				abilities = [WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 4) {
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 5) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 6) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 7) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 8) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 9) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level >= 10) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, WROK, MOVE, CARRY, MOVE];
+			}
+		break;
 		case 'CreepBuilder':
 			if(level <= 1) {
 				abilities = [WORK, CARRY, MOVE];
