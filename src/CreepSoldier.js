bac2598868578abe972f0ba62617d7087a2fc8c7
Ferdinand Berger <ferdy2003@gmail.com>
2019-12-20 20:35:29 +0100

    ?

--- src/CreepSoldier.js
+++ src/CreepSoldier.js
@@ -17,7 +17,7 @@ CreepSoldier.prototype.init = function() {
 
 CreepSoldier.prototype.act = function() {
 
-    if(this.stayFlag()) { return true; }
+    //if(this.stayFlag()) { return true; }
     if(this.attackHostiles()) { return true; }
     if(this.attackSpawns()) { return true; }
     
