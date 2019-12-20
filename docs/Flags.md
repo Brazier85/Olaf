4a5f397ba964fdd47494fdfbd155bff43bd6fe06
Ferdinand Berger <bergefe@deagomac0007.fritz.box>
2019-12-20 10:29:50 +0100

    smarter flag usage

--- docs/Flags.md
+++ docs/Flags.md
@@ -13,9 +13,7 @@ Used to define some positions in the room. For example mining positions.
 
 | Color  | Type |
 | ------------- | ------------- |
-| ORANGE + RED  | First source position  |
-| ORANGE + PURPLE | Harvest pos1 for source 1 |
-| ORANGE + BLUE | Harvest pos2 for source 1 |
-| ORANGE + CYAN | Second source position |
-| ORANGE + GREEN | Harvest pos1 for source 2 |
-| ORANGE + YELLOW | Harvest pos2 for source 2 |
+| ORANGE + RED  | Harvest pos1 for source 1  |
+| ORANGE + PURPLE | Harvest pos2 for source 1 |
+| ORANGE + BLUE | Harvest pos1 for source 2 |
+| ORANGE + CYAN | Harvest pos1 for source 2 |
--- src/Flags.js
+++ src/Flags.js
@@ -32,18 +32,13 @@ FlagsController.run = function(rooms, flags) {
       _.forEach(orangeFlags, function(flagObject){
         var flag = Game.flags[flagObject.name]
 
-        // Flag on Source
-        if(flag.secondaryColor == COLOR_RED || flag.secondaryColor == COLOR_CYAN) {
-            setSource(flag);
-        }
-
         // Source Pos 1
-        if(flag.secondaryColor == COLOR_PURPLE || flag.secondaryColor == COLOR_GREEN) {
+        if(flag.secondaryColor == COLOR_RED || flag.secondaryColor == COLOR_PURPLE) {
             setPos(flag, 1);
         }
 
         // Source Pos 2
-        if(flag.secondaryColor == COLOR_BLUE || flag.secondaryColor == COLOR_YELLOW) {
+        if(flag.secondaryColor == COLOR_BLUE || flag.secondaryColor == COLOR_CYAN) {
             setPos(flag, 2);
         }
 
@@ -115,12 +110,26 @@ setSource = function(flag) {
 setPos = function(flag, position) {
     var room = Game.rooms[flag.pos.roomName];
     var source = flag.pos.findClosestByRange(FIND_SOURCES);
-    source.memory = room.memory.sources[source.id];
+
+    if(!room.memory.sources){ // If there is no sources memory
+        room.memory.sources = {}; //Add it
+    }
+
+    if(!room.memory.sources[source.id]){
+        source.memory = room.memory.sources[source.id] = {}; //Adding memory for source
+    } else {
+        source.memory = room.memory.sources[source.id];
+    }
+
     if (position == 1) {
         source.memory.pos1 = flag.pos;
+        console.log("Position set!");
+        flag.remove();
     }
     if (position == 2) {
         source.memory.pos2 = flag.pos;
+        console.log("Position set!");
+        flag.remove();
     }
 }
 
