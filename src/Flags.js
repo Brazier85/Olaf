8a859c801d4d1345d9ecc75e99aca5a55b796ad7
Ferdinand Berger <ferdy2003@gmail.com>
2019-12-22 00:31:45 +0100

    bugfix

--- src/Flags.js
+++ src/Flags.js
@@ -52,7 +52,6 @@ FlagsController.run = function(rooms, flags) {
         var flag = Game.flags[flagObject.name];
 
         if(flag.secondaryColor == COLOR_RED) {
-            flag.pos.findClosestByRange(FIND_HOSTILE_CREEP);
             flag.room.memory.roomTarget = flag.pos.findClosestByRange(FIND_HOSTILE_CREEP).id;
             flag.remove();
         }
